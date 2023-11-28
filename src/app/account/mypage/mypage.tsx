7a1ba580fb8e4887b2d9dd3f1ff378e8031d298d
崎山勇気 <sakiyamayuuki@sakiyamayuukinoMacBook-Pro.local>
2023-11-28 22:22:00 +0900

    UIの修正

--- src/app/account/mypage/mypage.tsx
+++ src/app/account/mypage/mypage.tsx
@@ -7,60 +7,137 @@ import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
 import Link from "next/link";
-import { Button } from "@mui/material";
+import { Button, Container, Stack, CardMedia } from "@mui/material";
+import { Database } from "../../../../database.types";
+import format from "date-fns/format";
+import GlobalHeader from "@/components/globalHeader";
+
+type Event = Database["public"]["Tables"]["events"]["Row"];
+type Participate = Database["public"]["Tables"]["event_participate"]["Row"];
 
 const MyAccount = ({ session }: { session: Session | null }) => {
   const user = session?.user;
   const userId = user && user.id;
-  const [participatedEvent, setParticipatedEvent] = useState<any>([]);
-  const [hostEvent, setHostEvent] = useState<any>([]);
+  const [participatedEventId, setParticipatedEventId] = useState<
+    Array<Participate> | undefined
+  >([]);
+  const [participatedEvent, setParticipatedEvent] = useState<
+    Array<Event> | undefined
+  >([]);
+
+  const [hostEvent, setHostEvent] = useState<Array<Event> | undefined>([]);
 
   useEffect(() => {
     const fetchEvent = async () => {
-      try {
-        const { data: eventId, error } = await supabase
-          .from("event_participate")
-          .select(
-            `
-          participated_event_id, 
-          events ( id, title,start_date,capacity,place )`
-          )
-          .eq("participating_account_id", userId);
-        const { data: hostingEvent } = await supabase
-          .from("events")
-          .select(
-            `
-          id,title,capacity,start_date,start_time,end_date,end_time,
-          event_participate ( id )`
-          )
-          .eq("host_id", userId);
-        if (error) {
-          throw error;
-        }
-        setParticipatedEvent(eventId);
+      const query = supabase
+        .from("event_participate")
+        .select("id,participated_event_id,participating_account_id")
+        .eq("participating_account_id", userId as string);
+      const { data: eventId } = await query;
+      if (eventId) {
+        setParticipatedEventId(eventId);
+      }
+      const { data: hostingEvent } = await supabase
+        .from("events")
+        .select(
+          "id,title,description,capacity,start_date,start_time,end_date,end_time,host_id,is_published,place,place_link,image_url"
+        )
+        .eq("host_id", userId as string);
+      if (hostingEvent) {
         setHostEvent(hostingEvent);
-      } catch (error) {
-        console.error("Error fetching data:", error);
+      }
+      const { data: ParticipatingEvent } = await supabase
+        .from("events")
+        .select(
+          "id,title,description,capacity,start_date,start_time,end_date,end_time,host_id,is_published,place,place_link,image_url"
+        )
+        .eq("host_id", userId as string);
+      if (ParticipatingEvent) {
+        setParticipatedEvent(ParticipatingEvent);
       }
     };
     fetchEvent();
   }, []);
-  console.log(hostEvent && hostEvent);
-  const hostedEvent = hostEvent && hostEvent;
-  // console.log(participatedEvent && participatedEvent);
-  // console.log(hostedEvent);
+
   return (
     <>
-      {" "}
       <div>
-        <div>主催しているイベント</div>
-        <div>
-          {hostedEvent?.map((event: any) => (
-            <div key={event.id}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardContent>
+        <GlobalHeader />
+      </div>{" "}
+      <div>
+        <Container component="main" fixed sx={{ mt: 10 }}>
+          <Typography gutterBottom variant="h4" component="div">
+            主催しているイベント
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            {hostEvent &&
+              hostEvent.map((event: Event) => (
+                <div key={event.id}>
+                  <Card sx={{ maxWidth: 400 }}>
+                    <div>
+                      {" "}
+                      <CardMedia
+                        component="img"
+                        sx={{ height: 140 }}
+                        image={event.image_url || ""}
+                        alt="イベントの表紙"
+                      />
+                    </div>
+                    <CardContent>
+                      <Typography gutterBottom variant="h5" component="div">
+                        <Link href="/event/[id]" as={`/event/${event.id}`}>
+                          {event.title}
+                        </Link>
+                      </Typography>
+                      <Typography
+                        variant="body2"
+                        color="text.secondary"
+                        component="div"
+                      >
+                        <div>
+                          {format(
+                            new Date(event.start_date || ""),
+                            "yyyy/MM/dd"
+                          )}
+                        </div>
+                      </Typography>
+                      <CardActions>
+                        {" "}
+                        <Button variant="contained">
+                          <Link
+                            href="/event/[id]/edit"
+                            as={`/event/${event.id}/edit`}
+                          >
+                            {event.is_published == true ? (
+                              <>編集</>
+                            ) : (
+                              <>下書きを編集</>
+                            )}
+                          </Link>
+                        </Button>
+                      </CardActions>
+                    </CardContent>
+                  </Card>
+                </div>
+              ))}
+          </Stack>
+          <Typography gutterBottom variant="h4" component="div">
+            参加予定のイベント{" "}
+          </Typography>
+          <Stack direction="row" spacing={2}>
+            {participatedEvent?.map((event: Event) => (
+              <div key={event.id}>
+                <Card sx={{ maxWidth: 400 }}>
                   <div>
                     {" "}
+                    <CardMedia
+                      component="img"
+                      sx={{ height: 140 }}
+                      image={event.image_url || ""}
+                      alt="イベントの表紙"
+                    />
+                  </div>
+                  <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                       <Link href="/event/[id]" as={`/event/${event.id}`}>
                         {event.title}
@@ -71,71 +148,16 @@ const MyAccount = ({ session }: { session: Session | null }) => {
                       color="text.secondary"
                       component="div"
                     >
-                      {event.date}
+                      <div>
+                        {format(new Date(event.start_date || ""), "yyyy/MM/dd")}
+                      </div>
                     </Typography>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      component="div"
-                    >
-                      参加者数：{event.event_participate.length}/
-                      {event.capacity}
-                      {/* {event.capacity} */}
-                    </Typography>
-                    <CardActions>
-                      {" "}
-                      <Button variant="contained">
-                        <Link
-                          href="/event/[id]/edit"
-                          as={`/event/${event.id}/edit`}
-                        >
-                          {event.is_published == true ? (
-                            <>編集</>
-                          ) : (
-                            <>下書きを編集</>
-                          )}
-                        </Link>
-                      </Button>
-                    </CardActions>
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
-          ))}
-        </div>
-        <div>参加予定のイベント</div>
-        <div>
-          {participatedEvent?.map((event: any) => (
-            <div key={event.events.id}>
-              <Card sx={{ maxWidth: 345 }}>
-                <CardContent>
-                  <div>
-                    {" "}
-                    <Typography gutterBottom variant="h5" component="div">
-                      <Link href="/event/[id]" as={`/event/${event.events.id}`}>
-                        {event.events.title}
-                      </Link>
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      component="div"
-                    >
-                      {event.events.date}
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      color="text.secondary"
-                      component="div"
-                    >
-                      {event.events.place}
-                    </Typography>
-                  </div>
-                </CardContent>
-              </Card>
-            </div>
-          ))}
-        </div>
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
+          </Stack>
+        </Container>
       </div>
     </>
   );
